e277799e6bc63885c3311275a4990ee365787bbf
Kephas7 <diyalisimon7@gmail.com>
2025-12-10 20:39:06 +0545

    Bottom NavigationBar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final User? user = FirebaseAuth.instance.currentUser;
@@ -28,59 +33,26 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
 
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(25),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.account_circle,
-                size: isLandscape ? 80 : 120,
-                color: Colors.black,
-              ),
-
-              const SizedBox(height: 20),
-
-              Text(
-                "Welcome,",
-                style: TextStyle(
-                  fontSize: isLandscape ? 22 : 26,
-                  color: Colors.grey.shade700,
-                ),
-              ),
-
-              const SizedBox(height: 8),
-
-              Text(
-                user?.displayName ?? "User",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: isLandscape ? 24 : 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              const SizedBox(height: 30),
-
-              SizedBox(
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    await FirebaseAuth.instance.signOut();
-                    Navigator.pushReplacementNamed(context, '/login');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                  ),
-                  child: const Text("Logout"),
-                ),
-              ),
-            ],
+      body: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.black),
+            label: "Home",
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.work, color: Colors.black),
+            label: "Gigs",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, color: Colors.black),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.black),
+            label: "Profile",
+          ),
+        ],
       ),
     );
   }
