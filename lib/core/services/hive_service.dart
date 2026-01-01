4df11e177caf99a6f65c7b972320efd024f100f4
Kephas7 <diyalisimon7@gmail.com>
2026-01-02 00:16:27 +0545

    Hive service done

---
+++ lib/core/services/hive_service.dart
@@ -0,0 +1,76 @@
+import 'package:getagig/core/constants/hive_table_constant.dart';
+import 'package:getagig/features/auth/data/models/auth_hive_model.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HiveService {
+  Future<void> init() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/${HiveTableConstant.dbName}';
+    Hive.init(path);
+    _registerAdapters();
+    await _openBoxes();
+  }
+
+  void _registerAdapters() {
+    if (!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
+      Hive.registerAdapter(AuthHiveModelAdapter());
+    }
+  }
+
+  Future<void> _openBoxes() async {
+    await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTable);
+  }
+
+  Future<void> _close() async {
+    await Hive.close();
+  }
+
+  Box<AuthHiveModel> get _authBox =>
+      Hive.box<AuthHiveModel>(HiveTableConstant.authTable);
+
+  // Register user
+  Future<AuthHiveModel> register(AuthHiveModel user) async {
+    await _authBox.put(user.userId, user);
+    return user;
+  }
+
+  // Login - find user by email and password
+  AuthHiveModel? login(String email, String password) {
+    try {
+      return _authBox.values.firstWhere(
+        (user) => user.email == email && user.password == password,
+      );
+    } catch (e) {
+      return null;
+    }
+  }
+
+  // Get user by ID
+  AuthHiveModel? getUserById(String userId) {
+    return _authBox.get(userId);
+  }
+
+  // Get user by email
+  AuthHiveModel? getUserByEmail(String email) {
+    try {
+      return _authBox.values.firstWhere((user) => user.email == email);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  // Update user
+  Future<bool> updateUser(AuthHiveModel user) async {
+    if (_authBox.containsKey(user.userId)) {
+      await _authBox.put(user.userId, user);
+      return true;
+    }
+    return false;
+  }
+
+  // Delete user
+  Future<void> deleteUser(String userId) async {
+    await _authBox.delete(userId);
+  }
+}
