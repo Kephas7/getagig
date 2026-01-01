d388b4a741646a47cb692a37cc89b2583c07e375
Kephas7 <diyalisimon7@gmail.com>
2026-01-01 23:48:18 +0545

    auth_model done

---
+++ lib/features/auth/data/models/auth_hive_model.dart
@@ -0,0 +1,49 @@
+import 'package:getagig/core/constants/hive_table_constant.dart';
+import 'package:getagig/features/auth/domain/entities/auth_entity.dart';
+import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
+
+part 'auth_hive_model.g.dart';
+
+@HiveType(typeId: HiveTableConstant.authTypeId)
+class AuthHiveModel extends HiveObject {
+  @HiveField(0)
+  final String? userId;
+
+  @HiveField(1)
+  final String name;
+
+  @HiveField(2)
+  final String email;
+
+  @HiveField(3)
+  final String? password;
+
+  AuthHiveModel({
+    String? userId,
+    required this.name,
+    required this.email,
+    this.password,
+  }) : userId = userId ?? Uuid().v4();
+
+  factory AuthHiveModel.fromEntity(AuthEntity entity) {
+    return AuthHiveModel(
+      userId: entity.userId,
+      name: entity.name,
+      email: entity.email,
+      password: entity.password,
+    );
+  }
+  AuthEntity toEntity() {
+    return AuthEntity(
+      userId: password,
+      name: name,
+      email: email,
+      password: password,
+    );
+  }
+
+  static List<AuthEntity> toEntityList(List<AuthHiveModel> models) {
+    return models.map((model) => model.toEntity()).toList();
+  }
+}
