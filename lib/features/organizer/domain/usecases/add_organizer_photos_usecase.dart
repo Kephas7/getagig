aa4ed83479ae0e4c09044e46e344262bc04f9ac9
Kephas7 <diyalisimon7@gmail.com>
2026-03-05 01:51:09 +0545

    update lib/features/organizer/domain/usecases/add_organizer_photos_usecase.dart done

--- lib/features/organizer/domain/usecases/add_organizer_photos_usecase.dart
+++ lib/features/organizer/domain/usecases/add_organizer_photos_usecase.dart
@@ -1,9 +1,9 @@
-﻿import 'dart:io';
+import 'dart:io';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:getagig/core/error/failures.dart';
 import 'package:getagig/core/usecases/app_usecase.dart';
-import 'package:getagig/features/organizer/data/repositories/organizer_repository_impl.dart';
+import 'package:getagig/features/organizer/data/repositories/organizer_repository.dart';
 import 'package:getagig/features/organizer/domain/entities/organizer_entity.dart';
 import 'package:getagig/features/organizer/domain/repositories/organizer_repository.dart';
 
