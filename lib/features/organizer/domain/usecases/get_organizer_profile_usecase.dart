9c338e29e144ce761f6a488b7cf9bb9df235eb05
Kephas7 <diyalisimon7@gmail.com>
2026-03-05 01:51:10 +0545

    update lib/features/organizer/domain/usecases/get_organizer_profile_usecase.dart done

--- lib/features/organizer/domain/usecases/get_organizer_profile_usecase.dart
+++ lib/features/organizer/domain/usecases/get_organizer_profile_usecase.dart
@@ -1,7 +1,7 @@
-﻿import 'package:dartz/dartz.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:getagig/core/usecases/app_usecase.dart';
-import 'package:getagig/features/organizer/data/repositories/organizer_repository_impl.dart';
+import 'package:getagig/features/organizer/data/repositories/organizer_repository.dart';
 import 'package:getagig/features/organizer/domain/repositories/organizer_repository.dart';
 import 'package:getagig/core/error/failures.dart';
 import '../entities/organizer_entity.dart';
