33a3ba889ad88a9cc84a1a5f54745bd9533319f5
Kephas7 <diyalisimon7@gmail.com>
2026-03-05 01:51:05 +0545

    update lib/features/musician/domain/repositories/musician_repository.dart done

--- lib/features/musician/domain/repositories/musician_repository.dart
+++ lib/features/musician/domain/repositories/musician_repository.dart
@@ -9,31 +9,32 @@ abstract interface class IMusicianRepository {
   Future<Either<Failure, MusicianEntity>> createProfile(
     Map<String, dynamic> data,
   );
-  
+
   Future<Either<Failure, MusicianEntity>> getProfile();
-  
+
   Future<Either<Failure, MusicianEntity>> getProfileById(String id);
-  
+
   Future<Either<Failure, MusicianEntity>> updateProfile(
     Map<String, dynamic> data,
   );
-  
-  Future<Either<Failure, void>> deleteProfile();
-  
-  Future<Either<Failure, MusicianEntity>> uploadProfilePicture(File file);
-  
-  Future<Either<Failure, MusicianEntity>> addPhotos(List<File> files);
-  
-  Future<Either<Failure, MusicianEntity>> removePhoto(String photoUrl);
-  
-  Future<Either<Failure, MusicianEntity>> addVideos(List<File> files);
-  
-  Future<Either<Failure, MusicianEntity>> removeVideo(String videoUrl);
-  
-  Future<Either<Failure, MusicianEntity>> addAudio(List<File> files);
-  
-  Future<Either<Failure, MusicianEntity>> removeAudio(String audioUrl);
-  
-  Future<Either<Failure, MusicianEntity>> updateAvailability(bool isAvailable);
-}
 
+  Future<Either<Failure, void>> deleteProfile();
+
+  Future<Either<Failure, MusicianEntity>> uploadProfilePicture(File file);
+
+  Future<Either<Failure, MusicianEntity>> addPhotos(List<File> files);
+
+  Future<Either<Failure, MusicianEntity>> removePhoto(String photoUrl);
+
+  Future<Either<Failure, MusicianEntity>> addVideos(List<File> files);
+
+  Future<Either<Failure, MusicianEntity>> removeVideo(String videoUrl);
+
+  Future<Either<Failure, MusicianEntity>> addAudio(List<File> files);
+
+  Future<Either<Failure, MusicianEntity>> removeAudio(String audioUrl);
+
+  Future<Either<Failure, MusicianEntity>> updateAvailability(bool isAvailable);
+
+  Future<Either<Failure, MusicianEntity>> requestVerification();
+}
