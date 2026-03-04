b1167ed2c21e907990cc4b35486a8f9e0c7754d7
Kephas7 <diyalisimon7@gmail.com>
2026-03-05 01:51:10 +0545

    update lib/features/organizer/domain/usecases/create_organizer_profile_usecase.dart done

--- lib/features/organizer/domain/usecases/create_organizer_profile_usecase.dart
+++ lib/features/organizer/domain/usecases/create_organizer_profile_usecase.dart
@@ -1,9 +1,9 @@
-﻿import 'package:dartz/dartz.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:getagig/core/error/failures.dart';
 import 'package:getagig/core/usecases/app_usecase.dart';
-import 'package:getagig/features/organizer/data/repositories/organizer_repository_impl.dart';
+import 'package:getagig/features/organizer/data/repositories/organizer_repository.dart';
 import 'package:getagig/features/organizer/domain/repositories/organizer_repository.dart';
 import '../entities/organizer_entity.dart';
 
@@ -13,9 +13,7 @@ class CreateOrganizerProfileParams extends Equatable {
   final String contactPerson;
   final String phone;
   final String email;
-  final String city;
-  final String state;
-  final String country;
+  final String location;
   final String organizationType;
   final List<String> eventTypes;
   final String? website;
@@ -26,9 +24,7 @@ class CreateOrganizerProfileParams extends Equatable {
     required this.contactPerson,
     required this.phone,
     required this.email,
-    required this.city,
-    required this.state,
-    required this.country,
+    required this.location,
     required this.organizationType,
     required this.eventTypes,
     this.website,
@@ -41,11 +37,7 @@ class CreateOrganizerProfileParams extends Equatable {
       'contactPerson': contactPerson,
       'phone': phone,
       'email': email,
-      'location': {
-        'city': city,
-        'state': state,
-        'country': country,
-      },
+      'location': location,
       'organizationType': organizationType,
       'eventTypes': eventTypes,
     };
@@ -59,25 +51,23 @@ class CreateOrganizerProfileParams extends Equatable {
 
   @override
   List<Object?> get props => [
-        organizationName,
-        bio,
-        contactPerson,
-        phone,
-        email,
-        city,
-        state,
-        country,
-        organizationType,
-        eventTypes,
-        website,
-      ];
+    organizationName,
+    bio,
+    contactPerson,
+    phone,
+    email,
+    location,
+    organizationType,
+    eventTypes,
+    website,
+  ];
 }
 
 final createOrganizerProfileUseCaseProvider =
     Provider<CreateOrganizerProfileUseCase>((ref) {
-  final repository = ref.read(organizerRepositoryProvider);
-  return CreateOrganizerProfileUseCase(repository: repository);
-});
+      final repository = ref.read(organizerRepositoryProvider);
+      return CreateOrganizerProfileUseCase(repository: repository);
+    });
 
 class CreateOrganizerProfileUseCase
     implements UsecaseWithParms<OrganizerEntity, CreateOrganizerProfileParams> {
@@ -93,4 +83,3 @@ class CreateOrganizerProfileUseCase
     return _repository.createProfile(params.toJson());
   }
 }
-
