6bc49504ffd5526625972bca735a1e713cacdacd
Kephas7 <diyalisimon7@gmail.com>
2026-03-05 01:51:10 +0545

    update lib/features/organizer/domain/usecases/remove_organizer_verification_document_usecase.dart done

--- lib/features/organizer/domain/usecases/remove_organizer_verification_document_usecase.dart
+++ lib/features/organizer/domain/usecases/remove_organizer_verification_document_usecase.dart
@@ -1,9 +1,9 @@
-﻿import 'package:dartz/dartz.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:getagig/core/error/failures.dart';
 import 'package:getagig/core/usecases/app_usecase.dart';
-import 'package:getagig/features/organizer/data/repositories/organizer_repository_impl.dart';
+import 'package:getagig/features/organizer/data/repositories/organizer_repository.dart';
 import 'package:getagig/features/organizer/domain/entities/organizer_entity.dart';
 import 'package:getagig/features/organizer/domain/repositories/organizer_repository.dart';
 
