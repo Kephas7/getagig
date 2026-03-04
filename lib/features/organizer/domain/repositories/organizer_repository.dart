caa1d0459767bd19583b60a1e4c1f1aeb2d5cd5a
Kephas7 <diyalisimon7@gmail.com>
2026-03-05 01:51:09 +0545

    update lib/features/organizer/domain/repositories/organizer_repository.dart done

--- lib/features/organizer/domain/repositories/organizer_repository.dart
+++ lib/features/organizer/domain/repositories/organizer_repository.dart
@@ -7,35 +7,36 @@ abstract interface class IOrganizerRepository {
   Future<Either<Failure, OrganizerEntity>> createProfile(
     Map<String, dynamic> data,
   );
-  
+
   Future<Either<Failure, OrganizerEntity>> getProfile();
-  
+
   Future<Either<Failure, OrganizerEntity>> getProfileById(String id);
-  
+
   Future<Either<Failure, OrganizerEntity>> updateProfile(
     Map<String, dynamic> data,
   );
-  
+
   Future<Either<Failure, void>> deleteProfile();
-  
+
   Future<Either<Failure, OrganizerEntity>> uploadProfilePicture(File file);
-  
+
   Future<Either<Failure, OrganizerEntity>> addPhotos(List<File> files);
-  
+
   Future<Either<Failure, OrganizerEntity>> removePhoto(String photoUrl);
-  
+
   Future<Either<Failure, OrganizerEntity>> addVideos(List<File> files);
-  
+
   Future<Either<Failure, OrganizerEntity>> removeVideo(String videoUrl);
-  
+
   Future<Either<Failure, OrganizerEntity>> addVerificationDocuments(
     List<File> files,
   );
-  
+
   Future<Either<Failure, OrganizerEntity>> removeVerificationDocument(
     String documentUrl,
   );
-  
-  Future<Either<Failure, OrganizerEntity>> updateActiveStatus(bool isActive);
-}
 
+  Future<Either<Failure, OrganizerEntity>> updateActiveStatus(bool isActive);
+
+  Future<Either<Failure, OrganizerEntity>> requestVerification();
+}
