9772f3e0d7573c69e5c09e794795f78506084ad1
Kephas7 <diyalisimon7@gmail.com>
2026-03-05 01:50:54 +0545

    update lib/features/dashboard/data/models/conversation_model.g.dart done

--- lib/features/dashboard/data/models/conversation_model.g.dart
+++ lib/features/dashboard/data/models/conversation_model.g.dart
@@ -6,17 +6,17 @@ part of 'conversation_model.dart';
 // TypeAdapterGenerator
 // **************************************************************************
 
-class ConversationModelAdapter extends TypeAdapter<_$ConversationModelImpl> {
+class ConversationModelAdapter extends TypeAdapter<ConversationModel> {
   @override
   final int typeId = 1;
 
   @override
-  _$ConversationModelImpl read(BinaryReader reader) {
+  ConversationModel read(BinaryReader reader) {
     final numOfFields = reader.readByte();
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return _$ConversationModelImpl(
+    return ConversationModel(
       id: fields[0] as String?,
       participants: (fields[1] as List).cast<AuthApiModel>(),
       lastMessage: fields[2] as String?,
@@ -26,19 +26,19 @@ class ConversationModelAdapter extends TypeAdapter<_$ConversationModelImpl> {
   }
 
   @override
-  void write(BinaryWriter writer, _$ConversationModelImpl obj) {
+  void write(BinaryWriter writer, ConversationModel obj) {
     writer
       ..writeByte(5)
       ..writeByte(0)
       ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.participants)
       ..writeByte(2)
       ..write(obj.lastMessage)
       ..writeByte(3)
       ..write(obj.createdAt)
       ..writeByte(4)
-      ..write(obj.updatedAt)
-      ..writeByte(1)
-      ..write(obj.participants);
+      ..write(obj.updatedAt);
   }
 
   @override
@@ -56,9 +56,8 @@ class ConversationModelAdapter extends TypeAdapter<_$ConversationModelImpl> {
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$ConversationModelImpl _$$ConversationModelImplFromJson(
-        Map<String, dynamic> json) =>
-    _$ConversationModelImpl(
+ConversationModel _$ConversationModelFromJson(Map<String, dynamic> json) =>
+    ConversationModel(
       id: _readId(json, '_id') as String?,
       participants: (json['participants'] as List<dynamic>?)
               ?.map((e) => AuthApiModel.fromJson(e as Map<String, dynamic>))
@@ -73,8 +72,7 @@ _$ConversationModelImpl _$$ConversationModelImplFromJson(
           : DateTime.parse(json['updatedAt'] as String),
     );
 
-Map<String, dynamic> _$$ConversationModelImplToJson(
-        _$ConversationModelImpl instance) =>
+Map<String, dynamic> _$ConversationModelToJson(ConversationModel instance) =>
     <String, dynamic>{
       '_id': instance.id,
       'participants': instance.participants,
