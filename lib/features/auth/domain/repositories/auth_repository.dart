05f59e0269abf9472d28897dee4c2375e60d6b3a
Kephas7 <diyalisimon7@gmail.com>
2026-03-05 01:50:53 +0545

    update lib/features/auth/domain/repositories/auth_repository.dart done

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -5,8 +5,8 @@ import 'package:getagig/features/auth/domain/entities/auth_entity.dart';
 abstract interface class IAuthRepository {
   Future<Either<Failure, bool>> register(AuthEntity user);
   Future<Either<Failure, AuthEntity>> login(String email, String password);
+  Future<Either<Failure, String>> forgotPassword(String email);
+  Future<Either<Failure, String>> resetPassword(String token, String password);
   Future<Either<Failure, AuthEntity>> getCurrentUser();
   Future<Either<Failure, bool>> logout();
 }
-
-
